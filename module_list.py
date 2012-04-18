efd6b81dd9ce12a8b549f2910183ea9e34a955e8
David Roe <roed.math@gmail.com>
2012-04-18 14:48:01 -0700

    STARTED TRACKING sage library file sage/modular/all.py

--- module_list.py
+++ module_list.py
@@ -1148,15 +1148,8 @@ ext_modules = [
               sources = ['sage/modular/modsym/p1list.pyx'],
               libraries = ['gmp']),
 
-    Extension('sage.modular.overconvergent.pollack.dist', # should get removed eventually
+    Extension('sage.modular.overconvergent.pollack.dist',
               sources = ['sage/modular/overconvergent/pollack/dist.pyx'],
-              libraries = ['flint','gmp','zn_poly'],
-              extra_compile_args=['-std=c99', '-D_XPG6'],
-              include_dirs = [SAGE_INC + 'FLINT/'],
-              depends = flint_depends),
-
-    Extension('sage.modular.pollack_stevens.dist',
-              sources = ['sage/modular/pollack_stevens/dist.pyx'],
               libraries = ['flint','gmp','zn_poly'],
               extra_compile_args=['-std=c99', '-D_XPG6'],
               include_dirs = [SAGE_INC + 'FLINT/'],
--- sage/modular/overconvergent/all.py
+++ sage/modular/overconvergent/all.py
@@ -1,5 +1,3 @@
 from weightspace import  WeightSpace_constructor as pAdicWeightSpace
 
 from genus0 import OverconvergentModularForms
-
-from pollack.all import *
