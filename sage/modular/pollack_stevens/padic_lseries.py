68497d405e928901dab48fd8564247460365f205
Steffen Mueller <jansteffenmueller@gmail.com>
2012-04-20 18:32:38 -0700

    changed basic_integral, added twisted_symbol_on_Da

--- sage/modular/pollack_stevens/padic_lseries.py
+++ sage/modular/pollack_stevens/padic_lseries.py
@@ -23,6 +23,9 @@ class pAdicLseries(SageObject):
         """
         return self._symb.parent().prime()
 
+    if self.prime() == none:
+        raise ValueError ("Not a p-adic overconvergent modular symbol.")
+
     def _repr_(self):
         r"""
         Return print representation.
@@ -35,19 +38,56 @@ class pAdicLseries(SageObject):
         """
         pass
 
-    def basic_integral(self,a,j,ap,D=None):
+    def twisted_symbol_on_Da(self, a, D): # rename! should this be in modsym?
+        """
+        Returns `\Phi_{\chi}(\{a/p}-{\infty})` where `Phi` is the OMS
+        corresponding to self and `\chi` is a character of conductor `D`
+
+
+        INPUT:
+            - ``a`` -- integer in [0..p-1]
+            - ``D`` -- conductor of the quadratic twist `\chi`
+
+        OUTPUT:
+
+        `\Phi_{\chi}(\{a/p\}-\{\infty\})`
+
+        EXAMPLES:
+
+        """
+        symb = self.symb()
+        p = symb.parent().prime()
+        twisted_dist = symb.parent().zero_element()
+        m_map = symb._map
+        for b in range(1, abs(D) + 1):
+            if gcd(b, D) == 1:
+                M1 = M2Z([1, b / abs(D), 0, 1])
+                new_dist = m_map.__call__(M1 * M2Z[a, 1, p, 0])._act_right(M1)
+                new_dist = new_dist.scale(kronecker(D, b)).normalize()
+                twisted_dist = twisted_dist._add(new_dist)
+                #ans = ans + self.eval(M1 * M2Z[a, 1, p, 0])._right_action(M1)._lmul_(kronecker(D, b)).normalize()
+        return PSModularSybmolElement(twisted_dist.normalize(), symb.parent())
+
+    def basic_integral(self, a, j, D=None):
         r"""
         Returns `\int_{a+pZ_p} (z-{a})^j d\Phi(0-infty)`
         -- see formula [Pollack-Stevens, sec 9.2]
 
         """
+        if D == None:
+            D = 1
+        #check that a is between 0 and p - 1
         symb = self.symb()
-        M = symb.num_moments()
+        M = symb.precision_cap()
+        if j > M:
+            raise PrecisionError ("Too many moments for %s."%s(symb))
         p = self.prime()
-        K = Qp(p,M)
-        ap = ap*kronecker(D,p)
-        return sum(binomial(j,r)*((a- ZZ(K.teichmuller(a)))**(j-r))*(p**r)*self.phi_on_Da(symb,a,D).moment(r) for r in range(j+1))/ap
-
+        ap = symb.ap(p)
+        ap = ap * kronecker(D, p)
+        K = Qp(p, M)
+        symb_twisted = twisted_symbol_on_Da(symb, a, D)
+        return sum(binomial(j, r) * ((a - ZZ(K.teichmuller(a)))**(j - r)) *
+                (p**r) * self.phi_on_Da(a, D).moment(r) for r in range(j+1)) / ap
 
 def log_gamma_binomial(p,gamma,z,n,M):
     r"""
