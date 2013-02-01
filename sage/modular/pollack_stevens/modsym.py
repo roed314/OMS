roed314/OMS

commit 59e0b47dbc15e05fef8527ae21a88948eb9dccb9

added doctest

sage/modular/pollack_stevens/modsym.py
@@ -23,7 +23,7 @@ class PSModSymAction(Action):
 
     def _call_(self, sym, g):
         return sym.__class__(sym._map * g, sym.parent(), construct=True)
-
+    
 class PSModularSymbolElement(ModuleElement):
     def __init__(self, map_data, parent, construct=False):
         ModuleElement.__init__(self, parent)
@@ -121,7 +121,7 @@ class PSModularSymbolElement(ModuleElement):
         for val in self._map:
             val.normalize()
         return self
-
+    
     def __cmp__(self, other):
         """
         Checks if self == other
@@ -522,11 +522,11 @@ class PSModularSymbolElement(ModuleElement):
 class PSModularSymbolElement_symk(PSModularSymbolElement):
     def _find_M(self, M):
         """
-        Determines M from user input.
+        Determines `M` from user input.
 
         INPUT:
 
-        - ``M`` -- an integer at least 2 or None.  If None, sets M to
+        - ``M`` -- an integer at least 2 or None.  If None, sets `M` to
           be one more than the precision cap of the parent (the
           minimum amount of lifting).
 
@@ -548,6 +548,40 @@ class PSModularSymbolElement_symk(PSModularSymbolElement):
 
     def _find_alpha(self, p, k, M=None, ap=None, new_base_ring=None, ordinary=True, check=True, find_extraprec=True):
         """
+        Finds `alpha`, a `U_p` eigenvalue, which is found as a root of
+        the polynomial `x^2 - ap * x + p^(k+1)`.
+        
+        INPUT:
+
+        - ``p`` -- prime
+        - ``k`` -- Pollack-Stevens weight
+        - ``M`` -- precision (default = None) of `Q_p`
+        - ``ap`` -- Hecke eigenvalue at p (default = None)
+        - ``new_base_ring`` -- field of definition of `alpha` (default = None)
+        - ``ordinary`` -- True if the prime is ordinary (default = True)
+        - ``check`` -- check to see if the prime is ordinary (default = True)
+        - ``find_extraprec`` -- setting this to True finds extra precision (default = True)
+
+        OUTPUT:
+
+        - ``alpha`` --  `U_p` eigenvalue
+        - ``new_base_ring`` -- field of definition of `alpha` with precision at least `newM`
+        - ``newM`` -- new precision
+        - ``eisenloss`` -- loss of precision
+        - ``q`` -- a prime not equal to p which was used to find extra precision
+        - ``aq`` -- the Hecke eigenvalue `aq` corresponding to `q`
+
+        EXAMPLES::
+
+            sage: from sage.modular.pollack_stevens.space import ps_modsym_from_elliptic_curve
+            sage: E = EllipticCurve('11a')
+            sage: p = 5
+            sage: M = 10
+            sage: k = 0
+            sage: phi = ps_modsym_from_elliptic_curve(E)
+            sage: phi._find_alpha(p,k,M)
+            (1 + 4*5 + 3*5^2 + 2*5^3 + 4*5^4 + 4*5^5 + 4*5^6 + 3*5^7 + 2*5^8 + 3*5^9 + 3*5^10 + 3*5^12 + O(5^13), 5-adic Field with capped relative precision 13, 12, 1, None, None)
+
         """
         if ap is None:
             ap = self.Tq_eigenvalue(p, check=check)
@@ -637,7 +671,7 @@ class PSModularSymbolElement_symk(PSModularSymbolElement):
             Modular symbol with values in Sym^0 Q_5^2
             sage: phis.hecke(7) == phis*E.ap(7)
             True
-            sage: phis.hecke(5) == phis*E.ap(5)
+e            sage: phis.hecke(5) == phis*E.ap(5)
             False
             sage: phis.hecke(3) == phis*E.ap(3)
             True
@@ -1017,7 +1051,7 @@ class PSModularSymbolElement_dist(PSModularSymbolElement):
         Returns the underlying classical symbol of weight `k` -- i.e.,
         applies the canonical map `D_k --> Sym^k` to all values of
         self.
-
+        
         EXAMPLES::
 
             sage: D = Distributions(0, 5, 10);  M = PSModularSymbolSpace(Gamma0(2), D); M

commit 17b7a13cefe6faa271e2b6b574b235af2ebff7e6

Fixed tiny typo introduced in 59e0b47dbc15e05fef8527ae21a88948eb9dccb

sage/modular/pollack_stevens/modsym.py
@@ -680,7 +680,7 @@ class PSModularSymbolElement_symk(PSModularSymbolElement):
             Modular symbol with values in Sym^0 Q_5^2
             sage: phis.hecke(7) == phis*E.ap(7)
             True
-e            sage: phis.hecke(5) == phis*E.ap(5)
+            sage: phis.hecke(5) == phis*E.ap(5)
             False
             sage: phis.hecke(3) == phis*E.ap(3)
             True