b25ebbe560f5528579031371610edc13f2a202e4
Robert Pollack <rpollack@math.bu.edu>
2013-03-09 20:11:19 -0500

    more testing

--- sage/modular/pollack_stevens/modsym.py
+++ sage/modular/pollack_stevens/modsym.py
@@ -5,7 +5,7 @@
 #  as published by the Free Software Foundation; either version 2 of
 #  the License, or (at your option) any later version.
 #                  http://www.gnu.org/licenses/
-#*****************************************************************************
+#******************************************************************************
 
 import operator
 
